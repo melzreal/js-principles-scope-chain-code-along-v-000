557cc498d50272df72ab58c579ca0684f0b20f9c
Melissa Real <melissa.real@hotmail.com>
2019-03-19 16:57:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ function upperCaseCustomerName(){
 
 
 function setBestCustomer(){
+  const bestCustomer = 'not bob';
   return console.log('not bob');
 }
 
 function overWriteBestCustomer(){
   return console.log('maybe bob');
 }
+
+reassignLeastFavorite
