1cddc3b99f3229e7eeeac8a72d3dca8c0d4f2d27
Melissa Real <melissa.real@hotmail.com>
2019-03-19 15:38:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 /* Your code goes here */
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  console.log(customerName.toUpperCase());
+  return customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+  return 'not bob';
+}
