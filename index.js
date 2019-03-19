ebf314812e83032786177e5492891e0ff23b66fa
Melissa Real <melissa.real@hotmail.com>
2019-03-19 17:45:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 /* Your code goes here */
+
+var customerName = 'bob';
+
+  function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+  }
+
+
+  function setBestCustomer(){
+    return bestCustomer = 'not bob';
+
+  }
+
+  function overWriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+  }
+
+  const leastFavoriteCustomer = 'Dude';
+
+
+  function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Pickle';
+    return leastFavoriteCustomer;
+  }
+
+  function attemptTwoFavoriteCustomers() {
+    let favoriteCustomer = 'Mindy';
+    let favoriteCustomer = 'Shelly';
+    return favoriteCustomer;
+  }
